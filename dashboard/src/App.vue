e63270eab4c0bf7898d1f411755185f6f23adc45
Brandon Satrom <bsatrom+gh@gmail.com>
2021-02-27 12:47:35 -0600

    added Kendo UI components and sample

--- dashboard/src/App.vue
+++ dashboard/src/App.vue
@@ -1,32 +1,122 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <h1>Notecade!</h1>
+    <p>
+      <dropdownlist
+        :data-items="categories"
+        :data-item-key="'CategoryID'"
+        :text-field="'CategoryName'"
+        :default-item="defaultItems"
+        @change="handleDropDownChange"
+        >
+      </dropdownlist>
+      &nbsp; Selected category ID: <strong>{{this.dropdownlistCategory}}</strong>
+    </p>
+
+    <grid
+      :data-items="dataResult"
+      :pageable="pageable"
+      :sortable="sortable"
+      :sort="sort"
+      :skip="skip"
+      :take="take"
+      :columns="columns"
+      @datastatechange="dataStateChange"
+      :style="{ height: '400px' }"
+    >
+
+      <template v-slot:discontinuedTemplate="{ props }">
+              <td colspan="1">
+                    <input type="checkbox" :checked = props.dataItem.Discontinued disabled="disabled" />
+              </td>
+      </template>
+    </grid>
+
   </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import categories from './appdata/categories.json';
+import products from './appdata/products.json';
+import { process } from '@progress/kendo-data-query';
+import { Grid } from '@progress/kendo-vue-grid';
+import { DropDownList } from '@progress/kendo-vue-dropdowns';
+import '@progress/kendo-theme-material/dist/all.css';
 
-#nav {
-  padding: 30px;
-}
+export default {
+  name: 'App',
+  components: {
+    'dropdownlist': DropDownList,
+    'grid': Grid
+  },
+  data: function() {
+    return {
+      categories: categories,
+      products: products,
+      defaultItems: {CategoryID: null, CategoryName: "Product categories"},
+      dropdownlistCategory: null,
+      pageable: true,
+      sortable: true,
+      skip: 0,
+      take: 10,
+      sort: [
+          { field: "ProductName", dir: "asc" }
+      ],
+      filter: {},
+      columns: [
+          { field: 'ProductName', title: 'Product Name'},
+          { field: 'UnitPrice', title: 'Price' },
+          { field: 'UnitsInStock', title: 'Units in Stock' },
+          { field: 'Discontinued', cell: 'discontinuedTemplate' }
+      ],
+      dataResult:[]
+      }
+  },
+  created() {
+      const dataState = {
+          skip: this.skip,
+          take: this.take,
+          sort: this.sort,
+      };
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+      this.dataResult = process(products, dataState);
+  },
+  methods: {
+      handleDropDownChange (e) {
+          this.dropdownlistCategory = e.target.value.CategoryID;
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+          if (e.target.value.CategoryID !== null) {
+            this.filter = {
+              logic: 'and',
+              filters: [{ field: 'CategoryID', operator: 'eq', value: e.target.value.CategoryID }]
+            }
+            this.skip = 0
+          } else {
+            this.filter = []
+            this.skip = 0
+          }
+          let event = {data:{
+              skip: this.skip,
+              take: this.take,
+              sort: this.sort,
+              filter: this.filter
+          }};
+          this.dataStateChange(event);
+      },
+      createAppState: function(dataState) {
+          this.take = dataState.take;
+          this.skip = dataState.skip;
+          this.sort = dataState.sort;
+      },
+      dataStateChange (event) {
+          this.createAppState(event.data);
+          this.dataResult = process(products, {
+              skip: this.skip,
+              take: this.take,
+              sort: this.sort,
+              filter: this.filter
+          });
+      }
+  }
 }
-</style>
+</script>
--- dashboard/src/views/Home.vue
+++ dashboard/src/views/Home.vue
@@ -1,18 +1,16 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import { apiKey, apiUrl } from '@/consts.js'
+
+const reqUrl = `${apiUrl}?code=${apiKey}`
+
+console.log(reqUrl)
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+  name: 'Home'
 }
 </script>
